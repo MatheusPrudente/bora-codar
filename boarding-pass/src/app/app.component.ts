48f297aea947a025607fd618c96d2b21049a940c
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2023-02-20 21:37:08 -0300

    adicionar animacao no ticket de embarque

--- boarding-pass/src/app/app.component.ts
+++ boarding-pass/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { query } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'boarding-pass';
+
+  constructor () {}
+
+  ticketEffect(event : MouseEvent) {
+    const ticket = <HTMLElement> document.querySelector("#ticket");
+    const ticketWidth = ticket.offsetWidth;
+    const ticketHeight = ticket.offsetHeight;
+
+    const centerX = ticket.offsetLeft + ticketWidth/2;
+    const centerY = ticket.offsetTop + ticketHeight/2;
+
+    const positionX = event.clientX - centerX;
+    const positionY = event.clientY - centerY;
+
+    const rotateX = (+1) * 20 * positionY / (ticketHeight/2);
+    const rotateY = (-1) * 20 * positionX / (ticketWidth/2);
+
+    ticket.style.transform = `perspective(700px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  }
+
+
+  ticketBack() {
+    const ticket = <HTMLElement> document.querySelector("#ticket");
+    ticket.style.transform = `perspective(500px) rotateX(0deg) rotateY(0deg)`;
+    this.ticketTransition();
+  }
+
+  ticketTransition() {
+    /*const ticket = <HTMLBodyElement> document.querySelector("#ticket");
+    clearInterval(ticket.transitionId);
+
+    ticket.style.transition = 'transform 400ms';
+    ticket.transitionId = setTimeout(() => {
+      ticket.style.transition = '';
+    },400);*/
+  }
+
+  ticketEnter() {
+    this.ticketTransition();
+  }
 }
