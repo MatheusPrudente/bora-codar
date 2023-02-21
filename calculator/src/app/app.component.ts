50e87ebad5ca307d06cf940205d2fd8ec8e66ef7
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2023-02-21 20:47:38 -0300

    Adicionar funcoes na calculadora

--- calculator/src/app/app.component.ts
+++ calculator/src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+  mathematicalFormula : string = "";
+  resultMathematicalFormula : string = "";
+
+  constructor() {}
+
+  insert(caracter : string) {
+    this.mathematicalFormula = this.mathematicalFormula.concat(caracter);
+    this.previousResult();
+  }
+
+  remove() {
+    if(this.mathematicalFormula.length) {
+      this.mathematicalFormula = this.mathematicalFormula.slice(0,-1);
+    }
+  }
+
+  clearAll() {
+    this.mathematicalFormula = "";
+    this.resultMathematicalFormula = "";
+  }
+
+  previousResult() {
+    let formula = this.mathematicalFormula.replace(",",".");
+    this.resultMathematicalFormula = eval(formula).toString().replace(".",",");
+  }
+
+  result() {
+    this.mathematicalFormula = "";
+  }
+
+  changeSinal() {
+
+  }
 }
