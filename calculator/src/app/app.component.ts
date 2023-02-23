92ed0d36038015ef45f4fa6cb6876aca1c47d923
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2023-02-23 19:55:57 -0300

    Ajustes na calculadora

--- calculator/src/app/app.component.ts
+++ calculator/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface Dictionary<Type> {
+  [key: string]: Type;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,31 +13,77 @@ export class AppComponent {
   title = 'calculator';
   mathematicalFormula : string = "";
   resultMathematicalFormula : string = "";
+  symbols: Dictionary<string> = {
+    "+" : "+",
+    "-" : "-",
+    "÷" : "/",
+    "," : ".",
+    "x" : "*",
+    "%" : "%"
+  };
 
   constructor() {}
 
   insert(caracter : string) {
-    this.mathematicalFormula = this.mathematicalFormula.concat(caracter);
-    this.previousResult();
+    if(this.checkRules(caracter)) {
+      this.mathematicalFormula = this.mathematicalFormula.concat(caracter);
+      this.previousResult();
+    }
   }
 
   remove() {
     if(this.mathematicalFormula.length) {
       this.mathematicalFormula = this.mathematicalFormula.slice(0,-1);
+      this.previousResult();
     }
   }
 
-  clearAll() {
+  removeAll() {
     this.mathematicalFormula = "";
     this.resultMathematicalFormula = "";
   }
 
   previousResult() {
-    let formula = this.mathematicalFormula.replace(",",".");
-    this.resultMathematicalFormula = eval(formula).toString().replace(".",",");
+    if (this.mathematicalFormula.length > 0) {
+      const formula = this.replaceSymbols(this.mathematicalFormula);
+      console.log(formula);
+      const result = eval(formula);
+      console.log(result);
+      const resultString = result.toString().replace(".",",")
+      this.resultMathematicalFormula = resultString;
+    } else {
+      this.removeAll();
+    }
+  }
+
+  replaceSymbols(formula : string) {
+    formula = formula.replaceAll(",",this.symbols[","]);
+    formula = formula.replaceAll("÷",this.symbols["÷"])
+    formula = formula.replaceAll("x",this.symbols["x"])
+    return formula;
   }
 
   result() {
     this.mathematicalFormula = "";
+  }
+
+  checkRules(caracter : string) {
+    if (this.symbols[caracter]) {
+      if (this.mathematicalFormula.length <= 0) {
+        return false;
+      }
+      if(!this.isNumber(this.mathematicalFormula.at(-1))) {
+        return false;
+      }
+
+    }
+
+    return true
+  }
+
+  isNumber(value: string | undefined) {
+     return ((value != null) &&
+             (value !== '') &&
+             !isNaN(Number(value.toString())));
   }
 }
