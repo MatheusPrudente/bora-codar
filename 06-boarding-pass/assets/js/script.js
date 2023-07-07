f08378c19665c946751d79a5551aaf5a2fd9d887
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2023-07-06 22:58:09 -0300

    ajustes

--- 06-boarding-pass/assets/js/script.js
+++ 06-boarding-pass/assets/js/script.js
@@ -1,1 +1,48 @@
-console.log("Hello Word!!!");
+const ticket = document.querySelector("#ticket");
+const ticketDate = ticket.querySelector(".ticket-date");
+ticketDate.textContent = getCurrentDateFormatted();
+
+ticket.addEventListener("mousemove", (event) => {
+  const ticketWidth = ticket.offsetWidth;
+  const ticketHeight = ticket.offsetHeight;
+
+  const centerX = ticket.offsetLeft + ticketWidth/2;
+  const centerY = ticket.offsetTop + ticketHeight/2;
+
+  const positionX = event.clientX - centerX;
+  const positionY = event.clientY - centerY;
+
+  const rotateX = (+1)*25* positionY/(ticketHeight/2);
+  const rotateY = (-1)*25* positionX/(ticketWidth/2);
+
+  ticket.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+});
+
+ticket.addEventListener("mouseleave", (event) => {
+  ticket.style.transform = `rotateX(0deg) rotateY(0deg)`;
+  ticket.style.transition = "transform 300ms";
+
+  setTimeout(() => {
+    ticket.style.transition = "";
+  },400);
+});
+
+ticket.addEventListener("mouseenter", (event) => {
+  clearInterval(ticket.transitionId);
+  ticket.style.transition = "transform 300ms";
+
+  ticket.transitionId = setTimeout(() => {
+    ticket.style.transition = "";
+  },400);
+});
+
+function getCurrentDateFormatted(){
+  var currentDate = new Date(),
+  currentDay  = currentDate.getDate().toString(),
+  dayFormatted = (currentDay.length == 1) ? '0'+ currentDay : currentDay,
+  currentMonth  = (currentDate.getMonth()+1).toString(),
+  monthFormatted = (currentMonth.length == 1) ? '0'+ currentMonth : currentMonth,
+  yearFormatted = currentDate.getFullYear();
+  return dayFormatted + "/" + monthFormatted + "/" + yearFormatted;
+}
