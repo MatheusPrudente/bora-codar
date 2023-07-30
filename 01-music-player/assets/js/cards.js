4445f1b6ff7ce26cf843ec645fa0225e70b24b6d
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2023-07-29 22:44:00 -0300

    ajustes

--- 01-music-player/assets/js/cards.js
+++ 01-music-player/assets/js/cards.js
@@ -131,6 +131,9 @@ function createCardPlayer() {
 
   player.style.display = "flex";
 
+  let thumbnail = player.querySelector(".thumbnail");
+  thumbnail.src = selectMusic.thumbnail ? selectMusic.thumbnail : "https://images.unsplash.com/photo-1557672172-298e090bd0f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80";
+
   // name and band
   let info = player.querySelector(".info");
   info.querySelector("h1").innerHTML = selectMusic.name;
---
+++ 29-before-after-ia/README.md
@@ -0,0 +1,9 @@
+# 29-before-after-ia
+
+>Desenvolva um componente de antes e depois plataforma de melhoria de imagem com inteligência artificial
+
+
+## Tecnologias
+- HTML;
+- CSS;
+- JAVASCRIPT;
