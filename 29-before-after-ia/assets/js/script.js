ffcb108c02a6eaec188103d691145c96fb429caa
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2023-07-31 22:21:18 -0300

    ajustes

--- 29-before-after-ia/assets/js/script.js
+++ 29-before-after-ia/assets/js/script.js
@@ -1,1 +1,9 @@
-console.log("Hello Word!!!");
+const slider = document.querySelector("#slider");
+const beforeImage = document.querySelector(".before-image");
+const dragger = document.querySelector("#dragger");
+
+
+slider.addEventListener("input", (e) => {
+  beforeImage.style.width = e.target.value + '%';
+  dragger.style.left = `calc(${event.target.value}%)`
+})
